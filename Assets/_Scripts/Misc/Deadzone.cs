423eec2958ba6b75ebb7001607763203428930ac
ata kaleli <atakaleli123@gmail.com>
2025-02-22 09:15:29 +0300

    Update Deadzone.cs

--- Assets/_Scripts/Misc/Deadzone.cs
+++ Assets/_Scripts/Misc/Deadzone.cs
@@ -14,22 +14,15 @@ public class Deadzone : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(fallenObjectLayerMask == 1 << collision.gameObject.layer)
+        if ((fallenObjectLayerMask & (1 << collision.gameObject.layer)) != 0) // compare if the desired layer is matched
         {
             Agent agent = collision.GetComponent<Agent>();
 
-            if(agent == null)
-            {
-                Destroy(collision.gameObject);
-                return;
-            }
 
             var damagable = agent.GetComponent<Damagable>();
 
-
             if (damagable != null)
                 damagable.GetHit(agent.gameObject, 1);
-
 
             agent.AgentDied();
 
